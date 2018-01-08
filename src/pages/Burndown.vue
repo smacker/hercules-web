067e81f5d99160406da955da595ea84d40e16894
Maxim Sukharev <maxim@sourced.tech>
2018-01-08 15:11:36 +0100

    default resampling & error when not enough data

--- src/pages/Burndown.vue
+++ src/pages/Burndown.vue
@@ -147,15 +147,35 @@ export default {
           if (json.error) {
             return Promise.reject(json.error);
           }
+          if (json.data.data.length < 2) {
+            return Promise.reject('Not enough data');
+          }
+
           this.serverData = json.data.data;
           this.begin = json.data.begin;
           this.end = json.data.end;
+          this.resample = chooseDefaultResampling(this.begin, this.end);
         })
         .catch(e => (this.error = e))
         .then(() => (this.loading = false));
     }
   }
 };
+
+function chooseDefaultResampling(begin, end) {
+  begin = new Date(begin * 1000);
+  end = new Date(end * 1000);
+
+  const years = differenceInYears(end, begin);
+  if (years >= 3) {
+    return 'year';
+  }
+  const months = differenceInMonths(end, begin);
+  if (months > 3 && months < 36) {
+    return 'month';
+  }
+  return 'raw';
+}
 </script>
 
 <style scoped>
