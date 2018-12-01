d63428773fd8dbaebe31be9e4d60f9544396352f
Maxim Sukharev <max@smacker.ru>
2018-12-01 23:18:03 +0100

    improve error component

--- src/components/Error.vue
+++ src/components/Error.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="error">
     <h3>Oops! There is an error:</h3>
-    <p>{{ msg }}</p>
+    <p>{{ msg.toString() }}</p>
     <p>
       <a href="/">Try another repository</a>
     </p>
@@ -16,6 +16,7 @@ export default {
 
 <style scoped>
 .error {
+  text-align: center;
   color: #f5222d;
 }
 </style>
